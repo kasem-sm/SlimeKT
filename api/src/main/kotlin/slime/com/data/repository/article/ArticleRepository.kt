b0733cfe091bdfe9dc506ba1e49c5ed389f9b880
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-02-12 19:41:02 +0530

    Fix heroku issue by clearing build cache.

--- api/src/main/kotlin/slime/com/data/repository/article/ArticleRepository.kt
+++ api/src/main/kotlin/slime/com/data/repository/article/ArticleRepository.kt
@@ -3,22 +3,16 @@ package slime.com.data.repository.article
 import slime.com.data.models.Article
 
 interface ArticleRepository {
-
     suspend fun createArticle(article: Article): Boolean
-
     suspend fun deleteArticle(articleId: Int): Boolean
-
     suspend fun getArticleById(articleId: Int): Article?
-
     suspend fun getAllArticles(
         category: String = "",
         query: String,
         page: Int = 0,
         pageSize: Int = 10
     ): Pair<List<Article>, Int>
-
     suspend fun getRandomArticleFromSubscription(userId: String? = null): Article?
-
     suspend fun getRecommendedArticles(
         userId: String? = null,
         page: Int = 0,
--- api/src/main/kotlin/slime/com/data/repository/category/CategoryRepository.kt
+++ api/src/main/kotlin/slime/com/data/repository/category/CategoryRepository.kt
@@ -3,10 +3,7 @@ package slime.com.data.repository.category
 import slime.com.data.models.Category
 
 interface CategoryRepository {
-
     suspend fun getAllCategories(): List<Category>
-
     suspend fun insertCategory(category: Category): Boolean
-
     suspend fun getCategoryById(categoryId: String): Category?
 }
