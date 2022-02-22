831543f1945d4bcb7b67be4d5a998cba1c9ec447
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-02-22 07:05:51 +0530

    Updated dependencies

--- buildSrc/src/main/java/Hilt.kt
+++ buildSrc/src/main/java/Hilt.kt
@@ -1,8 +1,9 @@
 object Hilt {
-    private const val hiltVersion = "2.40.5"
+    private const val hiltVersion = "2.41"
 
     const val gradle = "com.google.dagger:hilt-android-gradle-plugin:$hiltVersion"
     const val library = "com.google.dagger:hilt-android:$hiltVersion"
     const val compiler = "com.google.dagger:hilt-compiler:$hiltVersion"
     const val navigation = "androidx.hilt:hilt-navigation-compose:1.0.0"
+    const val core = "com.google.dagger:hilt-core:$hiltVersion"
 }
