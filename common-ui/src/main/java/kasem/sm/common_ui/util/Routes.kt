8e845205ee9938d2edb2ce4d5f86f7f6b436881d
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-02-24 10:20:27 +0530

    Updated dependencies
    Updated dependencies & removed some unused deps in `common-ui` modules. Added

--- common-ui/src/main/java/kasem/sm/common_ui/util/Routes.kt
+++ common-ui/src/main/java/kasem/sm/common_ui/util/Routes.kt
@@ -6,65 +6,33 @@ package kasem.sm.common_ui.util
 
 import android.net.Uri
 import androidx.core.net.toUri
-import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavType
-import androidx.navigation.navArgument
 
 sealed class Routes(
     val route: String,
-    val arguments: List<NamedNavArgument> = emptyList(),
 ) {
+    object Main : Routes("main_route")
 
-    object Main : Routes(
-        route = "main_route"
-    )
+    object LoginScreen : Routes("login_screen")
 
-    object LoginScreen : Routes(
-        route = "login_screen"
-    )
+    object RegisterScreen : Routes("sign_up_screen")
 
-    object RegisterScreen : Routes(
-        route = "sign_up_screen",
-    )
-
-    object HomeScreen : Routes(
-        route = "home_screen"
-    )
+    object HomeScreen : Routes("home_screen")
 
     data class ExploreScreen(val query: String = "{slime_topic}") : Routes(
         route = "explore_screen/$query",
-        arguments = listOf(
-            navArgument("slime_topic") {
-                type = NavType.StringType
-            }
-        )
     )
 
-    object ProfileScreen : Routes(
-        route = "profile_screen"
-    )
+    object ProfileScreen : Routes("profile_screen")
 
-    object ArticleDetailScreen : Routes(
-        route = "article_detail_screen",
-    )
+    object ArticleDetailScreen : Routes("article_detail_screen")
 
-    object SubscribeTopicScreen : Routes(
-        route = "select_topics_screen"
-    )
+    object SubscribeTopicScreen : Routes("select_topics_screen")
 
     data class ListScreen(
         val topic: String = "{slime_topic}",
         val topicId: String = "{slime_topic_id}"
     ) : Routes(
         route = "list_screen/$topic/$topicId",
-        arguments = listOf(
-            navArgument("slime_topic") {
-                type = NavType.StringType
-            },
-            navArgument("slime_topic_id") {
-                type = NavType.StringType
-            }
-        )
     )
 
     companion object {
