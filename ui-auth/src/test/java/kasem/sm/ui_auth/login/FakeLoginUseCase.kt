be1a18bea65387437ccd55b2e337569dbdc5af50
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-03-09 20:51:53 +0530

    Test cleanups, fix lint, added proper git links in tests.md

--- ui-auth/src/test/java/kasem/sm/ui_auth/login/FakeLoginUseCase.kt
+++ ui-auth/src/test/java/kasem/sm/ui_auth/login/FakeLoginUseCase.kt
@@ -2,11 +2,7 @@
  * Copyright (C) 2022, Kasem S.M
  * All rights reserved.
  */
-<<<<<<< HEAD:ui-auth/src/test/java/kasem/sm/ui_auth/login/FakeLoginUseCase.kt
 package kasem.sm.ui_auth.login
-=======
-package kasem.sm.ui_login.ui
->>>>>>> dev:ui-auth/src/test/java/kasem/sm/ui_login/ui/FakeLoginUseCase.kt
 
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -18,18 +14,6 @@ import kotlinx.coroutines.flow.flowOf
 class FakeLoginUseCase {
     val mock: LoginUseCase = mockk()
 
-<<<<<<< HEAD:ui-auth/src/test/java/kasem/sm/ui_auth/login/FakeLoginUseCase.kt
-=======
-    fun mockAndThrowErr(
-        credentials: Credentials = Credentials(),
-        someException: Exception = SecurityException()
-    ) {
-        coEvery {
-            mock.execute(credentials)
-        } throws someException
-    }
-
->>>>>>> dev:ui-auth/src/test/java/kasem/sm/ui_login/ui/FakeLoginUseCase.kt
     fun mockAndReturn(
         result: AuthResult,
         credentials: Credentials = Credentials(),
