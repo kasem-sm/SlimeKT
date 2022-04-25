5b1c9d1053073d689fb2f6fb40eabe3793fad595
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-04-26 00:32:17 +0530

    one more cleanup

---
+++ features/article/datasource-api/src/main/java/kasem/sm/article/datasource/utils/ArticleData.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2022, Kasem S.M
+ * All rights reserved.
+ */
+package kasem.sm.article.datasource.utils
+
+data class DailyReadStatus(val dailyReadStatus: Boolean)
+
+data class IsActiveInDailyRead(val isActiveInDailyRead: Boolean)
+
+data class IsInExplore(val isInExplore: Boolean)
+
+data class IsBookmarked(val isBookmarked: Boolean)
