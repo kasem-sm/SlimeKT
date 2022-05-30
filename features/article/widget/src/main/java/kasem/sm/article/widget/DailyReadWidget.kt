5023cb0f174dd2fa5f0edf1356cccec77dc3ed57
kasem-sm <kasem-sm@github.com>
2022-05-30 06:56:56 +0530

    Change update mechanism of DailyReadWidget.kt (untested)

--- features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidget.kt
+++ features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidget.kt
@@ -5,12 +5,12 @@
 package kasem.sm.article.widget
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.action.ActionParameters
@@ -18,21 +18,26 @@ import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.action.ActionCallback
 import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.background
+import androidx.glance.currentState
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 
-class DailyReadWidget(
-    private val articleTitle: String = DEFAULT,
-) : GlanceAppWidget() {
+class DailyReadWidget : GlanceAppWidget() {
 
-    @RequiresApi(Build.VERSION_CODES.S)
+    override val stateDefinition = PreferencesGlanceStateDefinition
+
     @Composable
     override fun Content() {
+        val state = currentState<Preferences>()
+        val articleTitle = state[stringPreferencesKey("article_title")] ?: ""
+
         Column(
             modifier = GlanceModifier
                 .fillMaxSize()
@@ -49,15 +54,13 @@ class DailyReadWidget(
             )
         }
     }
-
-    companion object {
-        const val DEFAULT = ""
-    }
 }
 
 class ActionUpdate : ActionCallback {
-
     override suspend fun onRun(context: Context, glanceId: GlanceId, parameters: ActionParameters) {
-        DailyReadWidget("Updated from click").update(context, glanceId)
+        updateAppWidgetState(context, glanceId) { prefs ->
+            prefs[stringPreferencesKey("article_title")] = "Updated from Click"
+        }
+        DailyReadWidget().update(context, glanceId)
     }
 }
--- features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidgetReceiver.kt
+++ features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidgetReceiver.kt
@@ -5,8 +5,11 @@
 package kasem.sm.article.widget
 
 import android.content.Context
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.updateAll
 
 class DailyReadWidgetReceiver : GlanceAppWidgetReceiver() {
@@ -14,7 +17,13 @@ class DailyReadWidgetReceiver : GlanceAppWidgetReceiver() {
 
     companion object {
         suspend fun updateWidget(articleTitle: String, context: Context) {
-            DailyReadWidget(articleTitle).updateAll(context)
+            GlanceAppWidgetManager(context).getGlanceIds(DailyReadWidget::class.java)
+                .forEach { glanceId ->
+                    updateAppWidgetState(context, glanceId) { prefs ->
+                        prefs[stringPreferencesKey("article_title")] = articleTitle
+                    }
+                }
+            DailyReadWidget().updateAll(context)
         }
     }
 }
