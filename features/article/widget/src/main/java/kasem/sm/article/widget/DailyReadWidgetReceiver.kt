4aa24378015e836841118bbedde4158af364b93d
kasem-sm <kasem-sm@github.com>
2022-05-30 07:05:41 +0530

    Refactor on method of retrieving `glanceID`

--- features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidgetReceiver.kt
+++ features/article/widget/src/main/java/kasem/sm/article/widget/DailyReadWidgetReceiver.kt
@@ -10,20 +10,17 @@ import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.state.updateAppWidgetState
-import androidx.glance.appwidget.updateAll
 
 class DailyReadWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = DailyReadWidget()
 
     companion object {
         suspend fun updateWidget(articleTitle: String, context: Context) {
-            GlanceAppWidgetManager(context).getGlanceIds(DailyReadWidget::class.java)
-                .forEach { glanceId ->
-                    updateAppWidgetState(context, glanceId) { prefs ->
-                        prefs[stringPreferencesKey("article_title")] = articleTitle
-                    }
-                }
-            DailyReadWidget().updateAll(context)
+            val glanceId = GlanceAppWidgetManager(context).getGlanceIds(DailyReadWidget::class.java).last()
+            updateAppWidgetState(context, glanceId) { prefs ->
+                prefs[stringPreferencesKey("article_title")] = articleTitle
+            }
+            DailyReadWidget().update(context, glanceId)
         }
     }
 }
