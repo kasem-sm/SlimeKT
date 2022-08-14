abbf4f0b1ad3c0a39ffe4ef21ab2441ac7bf816d
rvenky125 <venkypaithireddy@gmail.com>
2022-08-14 09:12:34 +0530

    Fixes in the validation of register screen

--- features/authentication/domain/interactors/src/main/java/kasem/sm/authentication/domain/interactors/RegisterService.kt
+++ features/authentication/domain/interactors/src/main/java/kasem/sm/authentication/domain/interactors/RegisterService.kt
@@ -13,6 +13,7 @@ import kasem.sm.authentication.domain.model.Credentials
 import kasem.sm.authentication.domain.model.ServerException
 import kasem.sm.authentication.domain.model.containsOnlyNumbers
 import kasem.sm.authentication.domain.model.containsSpecialCharacters
+import kasem.sm.authentication.domain.model.containsWhiteSpaces
 import kasem.sm.core.utils.toMessage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -73,6 +74,8 @@ class RegisterService @Inject constructor(
             username.trim().length !in (4..10) -> "Username length should be between 4 to 10 characters".toInvalidCredentialsException()
             password.trim().length !in (4..20) -> "Password length should be between 4 to 20 characters".toInvalidCredentialsException()
             username.containsOnlyNumbers -> "Username should not only consists of numbers".toInvalidCredentialsException()
+            username.trim().containsWhiteSpaces -> "Spaces are not allowed inside username".toInvalidCredentialsException()
+            password.trim().containsWhiteSpaces -> "Spaces are not allowed inside password".toInvalidCredentialsException()
             username.containsSpecialCharacters -> "Special characters are not allowed inside username".toInvalidCredentialsException()
             else -> null
         }
--- features/authentication/domain/model/src/main/java/kasem/sm/authentication/domain/model/StringUtils.kt
+++ features/authentication/domain/model/src/main/java/kasem/sm/authentication/domain/model/StringUtils.kt
@@ -4,6 +4,8 @@
  */
 package kasem.sm.authentication.domain.model
 
-val String.containsSpecialCharacters get() = !(matches("[a-zA-Z0-9]+".toRegex()))
+val String.containsSpecialCharacters get() = !(matches("[a-zA-Z0-9\\s]+".toRegex()))
+
+val String.containsWhiteSpaces get() = contains("\\s".toRegex())
 
 val String.containsOnlyNumbers get() = (matches("[0-9]+".toRegex()))
