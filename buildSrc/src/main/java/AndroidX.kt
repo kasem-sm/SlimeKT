1fac236772010e3e9ddf58e1f77713b110e07784
Kasem SM <83884225+kasem-sm@users.noreply.github.com>
2022-10-06 09:00:18 +0530

    Updated dependencies (#218)

--- buildSrc/src/main/java/AndroidX.kt
+++ buildSrc/src/main/java/AndroidX.kt
@@ -2,10 +2,10 @@ object AndroidX {
     private const val gradleVersion = "7.2.1"
     const val gradle = "com.android.tools.build:gradle:$gradleVersion"
 
-    private const val coreVersion = "1.9.0-alpha05"
+    private const val coreVersion = "1.9.0"
     const val core = "androidx.core:core-ktx:$coreVersion"
 
-    private const val roomVersion = "2.5.0-alpha02"
+    private const val roomVersion = "2.5.0-beta01"
     const val room = "androidx.room:room-ktx:$roomVersion"
     const val roomRuntime = "androidx.room:room-runtime:$roomVersion"
     const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
@@ -14,9 +14,9 @@ object AndroidX {
     private const val navigation = "2.5.0-alpha02"
     const val navigationCommon = "androidx.navigation:navigation-common:$navigation"
 
-    private const val glanceVersion = "1.0.0-alpha03"
+    private const val glanceVersion = "1.0.0-alpha05"
     const val glance = "androidx.glance:glance-appwidget:$glanceVersion"
 
-    private const val annotationVersion = "1.4.0"
+    private const val annotationVersion = "1.5.0"
     const val annotation = "androidx.annotation:annotation:$annotationVersion"
 }
--- buildSrc/src/main/java/Firebase.kt
+++ buildSrc/src/main/java/Firebase.kt
@@ -2,6 +2,6 @@ object Firebase {
     private const val gmsVersion = "4.3.10"
     const val gmsGoogleService = "com.google.gms:google-services:$gmsVersion"
 
-    private const val dynamicLinksVersion = "21.0.1"
+    private const val dynamicLinksVersion = "21.0.2"
     const val dynamicLink = "com.google.firebase:firebase-dynamic-links-ktx:$dynamicLinksVersion"
 }
